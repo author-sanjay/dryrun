43f50c208d12572556c99712006f1eb3a03c98b7
sanjay <sanjaykumar73189@gmail.com>
2022-11-06 03:32:39 +0530

    profile tab done alost

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,16 @@
-import { Avatar } from "@material-ui/core";
+import { Avatar, Grid } from "@material-ui/core";
 import React from "react";
 
 const Home = () => {
+    
+      
   return (
     <section id="home" className="flex flex-row py-6">
       <section id="profile" className="flex-0.3 flex justify-center w-100">
-        <div className="flex flex-col items-center py-[6px] px-4 bg-profile-gradient rounded-[10px] mb-2 w-96 h-96">
-            <span  className="pt-10 font-poppins font-normal cursor-pointer text-[20px] text-white">My Profile</span>
+        <div className="flex flex-col items-center py-[6px] px-4 bg-profile-gradient rounded-[10px] mb-2 w-96 h-auto">
+          <span className="pt-10 font-poppins font-normal cursor-pointer text-[20px] text-white">
+            My Profile
+          </span>
           <div className="flex flex-column justify-self-auto">
             <div className="px-10 py-5 items-center">
               <Avatar
@@ -15,12 +19,26 @@ const Home = () => {
                 sizes="xxl"
                 src="https://flxt.tmsimg.com/assets/p18480237_b_v13_ac.jpg"
               />
-              
             </div>
-            
           </div>
-          <span className="pt-5 font-poppins font-normal cursor-pointer text-[20px] text-white">Lucifer</span>
+          <span className="pt-5 font-poppins font-normal cursor-pointer text-[20px] text-white">
+            Lucifer
+          </span>
+          <span className="pt-1 font-poppins font-normal text-[13px] text-white">
+            Software Developer @
+          </span>
+          <a
+            href=""
+            className=" font-poppins font-normal text-[18px] text-white mb-10"
+          >
+            Virtualz
+          </a>
+          <div>
+    
+          </div>
         </div>
+
+
       </section>
       <section id="quesion" className="flex-1 flex justify-center items-start">
         hghj
