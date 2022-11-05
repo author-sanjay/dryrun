5eecc6d45526593bba6d3acbf127294c94a81355
sanjay <sanjaykumar73189@gmail.com>
2022-11-06 04:26:08 +0530

    styles card

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     <section id="home" className="flex flex-row py-12">
       <section
         id="profile"
-        className="flex-0.3 flex mt-10 justify-center w-100"
+        className="flex-0.3 flex ml-10 mt-8 justify-center w-100"
       >
         <div className="flex flex-col items-center py-[6px] px-4 bg-profile-gradient rounded-[10px] mb-2 w-96 h-auto">
           <span className="pt-10 font-poppins font-normal cursor-pointer text-[20px] text-white">
@@ -34,13 +34,17 @@ const Home = () => {
           >
             Virtualz
           </a>
-          <div className="mb-10">
+          <div className="flex mb-2 justify-items-center">
+            <span className="font-poppins font-normal text-[20px] text-white" >Socials</span>
+          </div>
+          <div className="mb-6">
+          
             <table className="w-80 h-auto">
               <tr className="flex justify-between mb-2">
-                <td className="font-poppins font-normal text-[24px] text-white">
+                <td className="font-poppins text-white font-normal text-[24px] ">
                   Global Rank
                 </td>
-                <td className="font-poppins font-normal text-[24px] text-white">
+                <td className="font-poppins text-white font-normal text-[24px] ">
                   Not Defined
                 </td>
               </tr>
@@ -60,10 +64,47 @@ const Home = () => {
                   78.2%
                 </td>
               </tr>
+              <tr className="flex justify-between mb-2">
+                <td className="font-poppins font-normal text-[16px] text-white">
+                  Joined
+                </td>
+                <td className="font-poppins font-normal text-[16px] text-white">
+                  06 June 2022
+                </td>
+              </tr>
             </table>
           </div>
-          <div className="">
-        
+          <div className="flex mb-2 justify-items-start">
+            <span className="font-poppins font-normal text-[23px] text-white" >Socials</span>
+          </div>
+          <div className="items-center">
+            <table className="w-80 h-auto">
+              <tr className="flex justify-between mb-2">
+                <td className="font-poppins font-normal text-[16px] text-white">
+                  Icon
+                </td>
+                <td className="font-poppins font-normal text-[16px] text-white">
+                  <a href="">LinkedIn</a>
+                </td>
+              </tr>
+              <tr className="flex justify-between mb-2">
+                <td className="font-poppins font-normal text-[16px] text-white">
+                  Icon
+                </td>
+                <td className="font-poppins font-normal text-[16px] text-white">
+                <a href="">Github</a>
+                </td>
+              </tr>
+              <tr className="flex justify-between mb-2">
+                <td className="font-poppins font-normal text-[16px] text-white">
+                  Icon
+                </td>
+                <td className="font-poppins font-normal text-[16px] text-white">
+                <a href="">Instagram</a>
+                </td>
+              </tr>
+            </table>
+          
           </div>
         </div>
       </section>
